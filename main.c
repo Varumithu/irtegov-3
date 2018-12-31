a58fe70f76f2e52e0d2af3f20a75c2f38578bc7b
valmit <sliper98@gmail.com>
2018-12-31 12:46:07 +0700

    done

--- main.c
+++ main.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
 
 int main()
 {
-    printf("Hello World!\n");
+    uid_t real = getuid();
+    uid_t eff = geteuid();
+    printf("RUID = %d EUID = %d\n", real, eff);
+    FILE* file = fopen("/home/valmit/Documents/QtProjects/irtegov-3/secret.txt", "r+");
+    if (file == NULL) {
+        perror("Access Denied");
+    }
+    else fclose(file);
+    if (setuid(real) == -1) {
+        perror("setuid problem");
+    }
+    printf("RUID = %d EUID = %d\n", real, eff);
+    file = fopen("/home/valmit/Documents/QtProjects/irtegov-3/secret.txt", "r+");
+    if (file == NULL) {
+        perror("Access Denied");
+    }
+    else fclose(file);
     return 0;
 }
